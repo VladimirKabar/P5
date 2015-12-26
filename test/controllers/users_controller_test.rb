2726acf839569f27ad2b809911e3a20805a8c3e0
VladimirKabar <vladimirkabar@gmail.com>
2015-12-26 23:29:34 +0100

    Poprawiono strony startowe

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,6 +9,7 @@ class UsersControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
+    assert_select 'title', "Users | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get new" do
