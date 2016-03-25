9077ee6b33fe7a481a5215edc99859a45992998c
VladimirKabar <vladimirkabar@gmail.com>
2016-03-25 20:54:45 +0100

    walka z CarierWave .. dodano carriewave do initializers

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+        # Configuration for Amazon S3
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region => 'us-east-1',
+        :s3_host_name => 's3-eu-west-1.amazonaws.com'
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+  end
+end
