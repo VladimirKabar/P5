394c3f382cf6066580b6cfbbca06859b1b8d2fba
VladimirKabar <vladimirkabar@gmail.com>
2016-03-25 21:18:48 +0100

    q

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,8 @@ if Rails.env.production?
         :provider              => 'AWS',
         :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region => 'ue-west-1',
+        :s3_host_name => 's3-eu-west-1.amazonaws.com'
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
   end
